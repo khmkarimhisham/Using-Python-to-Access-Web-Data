72cfd0112bd0783ab1bfe5f738f5c367f027ebf4
Karim Hisham <khm.karimhisham@gmail.com>
2020-11-05 04:38:54 +0200

    week 4

---
+++ Week 4 - Assignment: Following Links in HTML Using BeautifulSoup.py
@@ -0,0 +1,21 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+url = input('Enter URL: ')
+count = int(input('Enter count: '))
+position = int(input('Enter position: '))-1
+
+for i in range(count):
+    html = urllib.request.urlopen(url, context=ctx).read()
+    soup = BeautifulSoup(html, 'html.parser')
+    tags = soup('a')
+    url = tags[position].get('href', None)
+    print("Retrieving: " + url)
